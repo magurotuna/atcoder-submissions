60bc24ff8d609660d88cdf1965618be00483571b
Yusuke Tanaka <wing0920@gmail.com>
2020-02-09 11:21:50 +0900

    abc136 D & abc137 D

--- abc136/src/bin/d.rs
+++ abc136/src/bin/d.rs
@@ -1,5 +1,68 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        S: String,
+    }
+    let chars = S.chars().collect::<Vec<_>>();
+    let chars_len = chars.len();
+    // ランレングス圧縮する
+    // rl[i] := (iが偶数なら)連続しているRの数、(iが奇数なら)連続しているLの数
+    let mut rl = Vec::with_capacity(chars.len());
+
+    let mut cur_c = chars[0];
+    let mut cur_len = 1;
+    for c in chars.into_iter().skip(1) {
+        if cur_c == c {
+            cur_len += 1;
+            continue;
+        } else {
+            cur_c = c;
+            rl.push(cur_len);
+            cur_len = 1;
+        }
+    }
+    rl.push(cur_len);
+    assert!(rl.len() % 2 == 0);
+
+    dbg!(&rl);
+
+    let mut ans_vec = Vec::with_capacity(chars_len);
+    for i in (0..(rl.len() - 1)).step_by(2) {
+        let r_num = rl[i];
+        let l_num = rl[i + 1];
+
+        for _ in 0..(r_num - 1) {
+            ans_vec.push(0);
+        }
+        if (r_num + l_num) % 2 == 0 {
+            ans_vec.push((r_num + l_num) / 2);
+            ans_vec.push((r_num + l_num) / 2);
+        } else {
+            // 平衡状態になるとき(max(r, l) - 1 回の移動が終わったタイミング)でceil((r + l) / 2) or floor((r + l) / 2)人がいる状態になる（rとlのどっちが大きいかで変わる）
+            // max(r, l) - 1 が偶数なら、10^100回の移動後はこの値そのままでOK. 奇数ならrとlが逆転する
+            let ceil = (r_num + l_num + 1) / 2;
+            let floor = (r_num + l_num) / 2;
+            use std::cmp::max;
+            if (max(r_num, l_num) - 1) % 2 == 0 {
+                ans_vec.push(if r_num > l_num { ceil } else { floor });
+                ans_vec.push(if r_num > l_num { floor } else { ceil });
+            } else {
+                ans_vec.push(if r_num > l_num { floor } else { ceil });
+                ans_vec.push(if r_num > l_num { ceil } else { floor });
+            }
+        }
+        for _ in 0..(l_num - 1) {
+            ans_vec.push(0);
+        }
+    }
+
+    println!(
+        "{}",
+        ans_vec
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
--- abc137/src/bin/d.rs
+++ abc137/src/bin/d.rs
@@ -1,5 +1,65 @@
 use libprocon::*;
+use std::cmp::Ordering;
+use std::collections::{HashMap, VecDeque};
 
+// AC 109ms
+// かなり複雑に考えすぎた。BinaryHeapでもっとシンプルに解けた。
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: usize,
+        AB: [(usize, usize); N],
+    }
+    // とりあえずソートしておく
+    // Aについての降順、Aが等しい場合はBについての降順、とする
+    let mut AB = AB;
+    AB.sort_by(|x, y| match y.0.cmp(&x.0) {
+        Ordering::Equal => y.1.cmp(&x.1),
+        other => other,
+    });
+    // dbg!(&AB);
+
+    // 方針
+    // 最終日の1日前から順にさかのぼりながら考える
+    // 最終日の1日前では、1日後に報酬がもらえるバイトのうち、一番報酬が高いものをやればOK
+    // 最終日の2日前では、1日後or2日後に報酬がもらえるバイトのうち、一番報酬が高いものをやればOK. ただし、最終日1日前にやったバイトはやることができないことに注意
+    // 以下同じように、貪欲にバイトを選択していけばよい。
+    //
+    // keyが「何日後に報酬がもらえるか」、valueがバイトの報酬のVec(降順）、となるようなHashMapを作る
+    // また、セグメント木を作り、1日後、2日後、...、i日後に報酬がもらえるバイトのうち、一番報酬が高いものはどれか、というクエリをlogオーダーで解決できるようにしておく
+    // これで O(nlogn) で解答できる はず。。。
+    let mut hm = HashMap::with_capacity(N);
+    let mut st = SegmentTree::new((0, 0), 100_000 + 1, |x, y| {
+        if x.0 >= y.0 {
+            (x.0, x.1)
+        } else {
+            (y.0, y.1)
+        }
+    });
+    for (day, compensation) in AB.into_iter() {
+        if !hm.contains_key(&day) {
+            st.update(day, (compensation, day));
+        }
+        let entry = hm.entry(day).or_insert(VecDeque::new());
+        entry.push_back(compensation);
+    }
+
+    // dbg!((
+    //     st.query(1, 2),
+    //     st.query(1, 3),
+    //     st.query(1, 4),
+    //     st.query(1, 5)
+    // ));
+
+    let mut ans = 0;
+    for i in 1..=M {
+        let res = st.query(1, i + 1);
+        ans += res.0;
+        if let Some(hm_el) = hm.get_mut(&res.1) {
+            hm_el.pop_front();
+            let next = hm.get(&res.1).unwrap().get(0).unwrap_or(&0);
+            st.update(res.1, (*next, res.1));
+        }
+    }
+    println!("{}", ans);
 }
