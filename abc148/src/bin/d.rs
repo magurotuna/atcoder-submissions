fcc5575fd31c67fe101dca92e6643c4fa6677c0f
Yusuke Tanaka <wing0920@gmail.com>
2020-02-18 09:33:28 +0900

    abc148 D

--- abc148/src/bin/d.rs
+++ abc148/src/bin/d.rs
@@ -1,5 +1,19 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        a: [usize; N],
+    }
+    // 1があればN-1個砕いて1を残せば満足させられる
+    // 1がないと絶対無理
+    // 1,2,3...というように1ずつ増加する部分列の長さを求める
+    let mut cnt = 0;
+    for i in 0..N {
+        if a[i] == cnt + 1 {
+            cnt += 1;
+        }
+    }
+    dbg!(cnt);
+    println!("{}", if cnt == 0 { -1 } else { N as i64 - cnt as i64 });
 }
