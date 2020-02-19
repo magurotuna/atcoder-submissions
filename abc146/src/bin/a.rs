90615016b340feda8d508b592e2a7c5181f30160
Yusuke Tanaka <wing0920@gmail.com>
2020-02-19 23:09:29 +0900

    abc146 A,B

--- abc146/src/bin/a.rs
+++ abc146/src/bin/a.rs
@@ -1,5 +1,10 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        S: String,
+    }
+    let d = vec!["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let i = d.iter().rev().position(|&x| x == S).unwrap();
+    println!("{}", i + 1);
 }
--- abc146/src/bin/b.rs
+++ abc146/src/bin/b.rs
@@ -1,5 +1,17 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: u8,
+        S: chars,
+    }
+    let cA = 'A' as u8;
+    dbg!(cA);
+
+    println!(
+        "{}",
+        S.into_iter()
+            .map(|c| ((((c as u8 - cA) + N) % 26) + cA) as char)
+            .collect::<String>()
+    );
 }
