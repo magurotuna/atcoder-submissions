c03d476dda47234badf60030114c8cc7c440469a
Yusuke Tanaka <wing0920@gmail.com>
2020-02-27 23:59:42 +0900

    abc124 A,B

--- abc124/src/bin/a.rs
+++ abc124/src/bin/a.rs
@@ -1,5 +1,26 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        a: i32,
+        b: i32,
+    }
+    let mut ans = 0;
+    let mut a = a;
+    let mut b = b;
+    ans += if a > b {
+        a -= 1;
+        a + 1
+    } else {
+        b -= 1;
+        b + 1
+    };
+    ans += if a > b {
+        a -= 1;
+        a + 1
+    } else {
+        b -= 1;
+        b + 1
+    };
+    println!("{}", ans);
 }
--- abc124/src/bin/b.rs
+++ abc124/src/bin/b.rs
@@ -1,5 +1,22 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        H: [i32; N],
+    }
+    let ans = 1
+        + (1..N)
+            .map(|n| {
+                let h = H[n];
+                if H.iter().take(n).all(|&t| dbg!(t) <= dbg!(h)) {
+                    dbg!((n, "a"));
+                    1
+                } else {
+                    dbg!((n, "b"));
+                    0
+                }
+            })
+            .sum::<i32>();
+    println!("{}", ans);
 }
