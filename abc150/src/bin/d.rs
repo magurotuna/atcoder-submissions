ece9f66715ff582612b09a5f3c986c5fa9363da0
Yusuke Tanaka <wing0920@gmail.com>
2020-02-28 23:31:41 +0900

    abc150 D

--- abc150/src/bin/d.rs
+++ abc150/src/bin/d.rs
@@ -1,5 +1,42 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: usize,
+        M: i64,
+        a: [i64; N],
+    }
+
+    let two_included = count_two(a[0]);
+    let mut l = 1;
+    for i in 0..N {
+        if two_included != count_two(a[i]) {
+            println!("0");
+            return;
+        }
+        l = lcm(l, a[i]);
+    }
+    let min_l = l / 2;
+    println!("{}", ((M / min_l) + 1) / 2);
+}
+
+fn count_two(n: i64) -> i64 {
+    let mut n = n;
+    let mut cnt = 0;
+    while n % 2 == 0 {
+        cnt += 1;
+        n /= 2;
+    }
+    cnt
+}
+
+pub fn gcd(a: i64, b: i64) -> i64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+pub fn lcm(a: i64, b: i64) -> i64 {
+    a / gcd(a, b) * b
 }
