9af5378d3d481df3092e8037b6d26283814efb29
Yusuke Tanaka <wing0920@gmail.com>
2020-02-29 23:05:38 +0900

    sumitrust2019 A,B,C,E

--- sumitrust2019/src/bin/b.rs
+++ sumitrust2019/src/bin/b.rs
@@ -1,5 +1,20 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: i64,
+    }
+    let mut ans = -1;
+    for x in 1..=n {
+        let v = x * 108 / 100;
+        dbg!((x, v));
+        if v == n {
+            ans = x;
+        }
+    }
+    if ans == -1 {
+        println!(":(");
+    } else {
+        println!("{}", ans);
+    }
 }
--- sumitrust2019/src/bin/c.rs
+++ sumitrust2019/src/bin/c.rs
@@ -1,5 +1,47 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        x: usize,
+    }
+    let mut dp = vec![false; x + 1];
+    dp[0] = true;
+
+    for i in 1..=x {
+        dp[i] = check(&dp, i);
+    }
+    println!("{}", if dp[x] { 1 } else { 0 });
+}
+
+fn check(table: &[bool], i: usize) -> bool {
+    (100..=105).map(|n| sub_check(table, i, n)).any(|b| b)
+}
+
+fn sub_check(table: &[bool], i: usize, sub: usize) -> bool {
+    match i.checked_sub(sub) {
+        Some(x) => *table.get(x).unwrap_or(&false),
+        None => false,
+    }
+}
+
+fn my_solution() {
+    input! {
+        x: i64,
+    }
+
+    if x >= 2000 {
+        println!("1");
+        return;
+    }
+
+    let r = x % 100;
+    let h = (x - r) / 100;
+
+    if r <= 5 * h {
+        println!("1");
+        return;
+    } else {
+        println!("0");
+        return;
+    }
 }
