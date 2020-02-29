bb12847485f95627e22a3274ffeb1efd06d19cc9
Yusuke Tanaka <wing0920@gmail.com>
2020-02-29 18:12:44 +0900

    abc155 E

--- abc155/src/bin/e.rs
+++ abc155/src/bin/e.rs
@@ -1,5 +1,24 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        N: chars,
+    }
+    let N: Vec<i64> = N
+        .into_iter()
+        .map(|n| (n as usize - '0' as usize) as i64)
+        .collect();
+
+    use std::cmp::min;
+    let mut eq = 0;
+    let mut plus1 = 1;
+    for i in 0..N.len() {
+        let n = N[i];
+        let e = eq;
+        let p = plus1;
+        eq = min(e + n, p + 10 - n);
+        plus1 = min(e + n + 1, p + 10 - n - 1);
+    }
+    dbg!(eq, plus1);
+    println!("{}", eq);
 }
