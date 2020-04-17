603da1012346657dc0957748aa5e9c521f230447
Yusuke Tanaka <yusuktan@maguro.dev>
2020-04-17 21:10:54 +0900

    abc149 B

--- abc149/src/bin/b.rs
+++ abc149/src/bin/b.rs
@@ -1,5 +1,11 @@
 use libprocon::*;
 
 fn main() {
-    unimplemented!();
+    input! {
+        A: i64,
+        B: i64,
+        K: i64,
+    }
+    use std::cmp::{max, min};
+    println!("{} {}", max(0, A - K), max(0, B - max(0, K - A)));
 }
