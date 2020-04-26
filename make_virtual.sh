775141713f9fb98b19e3f88f59bc026b96c136f0
Yusuke Tanaka <yusuktan@maguro.dev>
2020-04-26 18:50:04 +0900

    Fix make_virtual script

--- make_virtual.sh
+++ make_virtual.sh
@@ -10,66 +10,89 @@ fi
 cargo init $1
 cd $1
 git init
-mkdir src/bin
 
-cat <<'EOF' >> Cargo.toml
-libprocon = { git = "https://github.com/magurotuna/libprocon.git" }
-num = "0.2.1"
-num-bigint = "0.2.6"
-num-complex = "0.2.4"
-num-integer = "0.1.42"
-num-iter = "0.1.40"
-num-rational = "0.2.4"
-num-traits = "0.2.11"
-num-derive = "0.3.0"
-ndarray = "0.13.0"
-nalgebra = "0.20.0"
-alga = "0.9.3"
-libm = "0.2.1"
-rand = { version = "0.7.3", features = ["small_rng"] }
-getrandom = "0.1.14"
-rand_chacha = "0.2.2"
-rand_core = "0.5.1"
-rand_hc = "0.2.0"
-rand_pcg = "0.2.1"
-rand_distr = "0.2.2"
-petgraph = "0.5.0"
-indexmap = "1.3.2"
-regex = "1.3.6"
-lazy_static = "1.4.0"
-ordered-float = "1.0.2"
-ascii = "1.0.0"
-permutohedron = "0.2.4"
-superslice = "1.0.0"
-itertools = "0.9.0"
-itertools-num = "0.1.3"
-maplit = "1.0.2"
-either = "1.5.3"
-im-rc = "14.3.0"
-fixedbitset = "0.2.0"
-bitset-fixed = "0.1.0"
-proconio = { version = "0.3.6", features = ["derive"] }
-text_io = "0.1.8"
-whiteread = "0.5.0"
-rustc-hash = "1.1.0"
-smallvec = "1.2.0"
-EOF
+# FIXME: change this to "1.42.0" after the judge update is applied to old contests
+echo "1.15.1" > rust-toolchain
+mkdir src/bin
 
 for f in a.rs b.rs c.rs d.rs e.rs f.rs
 do
   cat <<'EOF' > src/bin/$f
-#![allow(unused_imports)]
-
-use proconio::fastout;
-use proconio::input;
-use proconio::marker::{Bytes, Chars, Usize1};
-
-#[fastout]
-fn main() {
-    input! {
-        r: f64,
+//! https://github.com/hatoo/competitive-rust-snippets
+//!
+//! MIT License
+//!
+//! Copyright (c) 2018 hatoo
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+#![allow(unused_imports, unused_macros, dead_code)]
+use std::cmp::{max, min, Ordering};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::io::{stdin, stdout, BufWriter, Write};
+use std::iter::FromIterator;
+mod util {
+    use std::fmt::Debug;
+    use std::io::{stdin, stdout, BufWriter, StdoutLock};
+    use std::str::FromStr;
+    pub fn line() -> String {
+        let mut line: String = String::new();
+        stdin().read_line(&mut line).unwrap();
+        line.trim().to_string()
     }
-    println!("{}", 2.0 * r * std::f64::consts::PI);
+    pub fn chars() -> Vec<char> {
+        line().chars().collect()
+    }
+    pub fn gets<T: FromStr>() -> Vec<T>
+    where
+        <T as FromStr>::Err: Debug,
+    {
+        let mut line: String = String::new();
+        stdin().read_line(&mut line).unwrap();
+        line.split_whitespace()
+            .map(|t| t.parse().unwrap())
+            .collect()
+    }
+    pub fn with_bufwriter<F: FnOnce(BufWriter<StdoutLock>) -> ()>(f: F) {
+        let out = stdout();
+        let writer = BufWriter::new(out.lock());
+        f(writer)
+    }
+}
+macro_rules !get {([$t :ty ] ) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;line .split_whitespace () .map (|t |t .parse ::<$t >() .unwrap () ) .collect ::<Vec <_ >>() } } ;([$t :ty ] ;$n :expr ) =>{(0 ..$n ) .map (|_ |get !([$t ] ) ) .collect ::<Vec <_ >>() } ;($t :ty ) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;line .trim () .parse ::<$t >() .unwrap () } } ;($($t :ty ) ,*) =>{{let mut line :String =String ::new () ;stdin () .read_line (&mut line ) .unwrap () ;let mut iter =line .split_whitespace () ;($(iter .next () .unwrap () .parse ::<$t >() .unwrap () ,) *) } } ;($t :ty ;$n :expr ) =>{(0 ..$n ) .map (|_ |get !($t ) ) .collect ::<Vec <_ >>() } ;($($t :ty ) ,*;$n :expr ) =>{(0 ..$n ) .map (|_ |get !($($t ) ,*) ) .collect ::<Vec <_ >>() } ;}
+macro_rules !debug {($($a :expr ) ,*) =>{eprintln !(concat !($(stringify !($a ) ," = {:?}, " ) ,*) ,$($a ) ,*) ;} }
+const BIG_STACK_SIZE: bool = true;
+fn main() {
+    use std::thread;
+    if BIG_STACK_SIZE {
+        thread::Builder::new()
+            .stack_size(32 * 1024 * 1024)
+            .name("solve".into())
+            .spawn(solve)
+            .unwrap()
+            .join()
+            .unwrap();
+    } else {
+        solve();
+    }
+}
+fn solve() {
+    todo!();
 }
 EOF
 done
