7e1089a77817e7809e434876e6656b58a9ff762e
Yusuke Tanaka <yusuktan@maguro.dev>
2020-05-03 16:18:43 +0900

    abc096 D

---
+++ abc096/src/bin/d.rs
@@ -0,0 +1,160 @@
+//! https://github.com/hatoo/competitive-rust-snippets
+//!
+//! MIT License
+//!
+//! Copyright (c) 2018 hatoo
+//!
+//! Permission is hereby granted, free of charge, to any person obtaining a copy
+//! of this software and associated documentation files (the "Software"), to deal
+//! in the Software without restriction, including without limitation the rights
+//! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//! copies of the Software, and to permit persons to whom the Software is
+//! furnished to do so, subject to the following conditions:
+//!
+//! The above copyright notice and this permission notice shall be included in all
+//! copies or substantial portions of the Software.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//! AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//! SOFTWARE.
+#![allow(
+    unused_imports,
+    unused_attributes,
+    unused_macros,
+    dead_code,
+    non_snake_case
+)]
+use std::cmp::{max, min, Ordering};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+use std::io::{stdin, stdout, BufWriter, Write};
+use std::iter::FromIterator;
+#[macro_export]
+macro_rules !get {(@inner [$src :expr ] chars ) =>{{let mut buf =String ::new () ;$src .read_line (&mut buf ) .unwrap () ;buf .trim () .chars () .collect ::<Vec <char >>() } } ;(@inner [$src :expr ] usize1 ) =>{{get !(@inner [$src ] usize ) -1 } } ;(@inner [$src :expr ] [usize1 ] ) =>{{get !(@inner [$src ] [usize ] ) .into_iter () .map (|v |v -1 ) .collect ::<Vec <usize >>() } } ;(@inner [$src :expr ] [[usize1 ] ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] [usize1 ] ) ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [usize1 ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] [usize1 ] ) ) .flatten () .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [[chars ] ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] chars ) ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [chars ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] chars ) ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [($($tt :tt ) ,*) ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] ($($tt ) ,*) ) ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] ($($tt :tt ) ,*) ) =>{{let mut buf :String =String ::new () ;$src .read_line (&mut buf ) .unwrap () ;let mut iter =buf .split_whitespace () ;($(get !(@inner_elem_parse [$tt ] iter .next () .unwrap () ) ,) *) } } ;(@inner [$src :expr ] [$t :ty ] ) =>{{let mut buf =String ::new () ;$src .read_line (&mut buf ) .unwrap () ;buf .trim () .split_whitespace () .map (|t |t .parse ::<$t >() .unwrap () ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [[$t :ty ] ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] [$t ] ) ) .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] [$t :ty ;$n :expr ] ) =>{{(0 ..$n ) .map (|_ |get !(@inner [$src ] [$t ] ) ) .flatten () .collect ::<Vec <_ >>() } } ;(@inner [$src :expr ] $t :ty ) =>{{let mut buf =String ::new () ;$src .read_line (&mut buf ) .unwrap () ;buf .trim () .split_whitespace () .next () .unwrap () .parse ::<$t >() .unwrap () } } ;(@inner_elem_parse [usize1 ] $elem :expr ) =>{{get !(@inner_elem_parse [usize ] $elem ) -1 } } ;(@inner_elem_parse [$t :ty ] $elem :expr ) =>{{$elem .parse ::<$t >() .unwrap () } } ;($tt :tt ) =>{{use std ::io ::BufRead ;let get_stdin =std ::io ::stdin () ;let mut locked_stdin =get_stdin .lock () ;get !(@inner [&mut locked_stdin ] $tt ) } } ;}
+macro_rules !debug {($($a :expr ) ,*$(,) *) =>{#[cfg (debug_assertions ) ] eprintln !(concat !($("| " ,stringify !($a ) ,"={:?} " ) ,*,"|" ) ,$(&$a ) ,*) ;} ;}
+macro_rules !echo {($($a :expr ) ,*) =>{let mut s =Vec ::new () ;$(s .push (format !("{}" ,$a ) ) ;) *println !("{}" ,s .join (" " ) ) ;} }
+#[macro_export]
+macro_rules !chmin {($base :ident ,$($cmps :expr ) ,+$(,) *) =>{$base =min !($base ,$($cmps ) ,+) ;} ;}
+#[macro_export]
+macro_rules !chmax {($base :ident ,$($cmps :expr ) ,+$(,) *) =>{$base =max !($base ,$($cmps ) ,+) ;} ;}
+#[macro_export]
+macro_rules !min {($a :expr ,$b :expr $(,) *) =>{{std ::cmp ::min ($a ,$b ) } } ;($a :expr ,$($rest :expr ) ,+$(,) *) =>{{std ::cmp ::min ($a ,min !($($rest ) ,+) ) } } ;}
+#[macro_export]
+macro_rules !max {($a :expr ,$b :expr $(,) *) =>{{std ::cmp ::max ($a ,$b ) } } ;($a :expr ,$($rest :expr ) ,+$(,) *) =>{{std ::cmp ::max ($a ,max !($($rest ) ,+) ) } } ;}
+const BIG_STACK_SIZE: bool = true;
+fn main() {
+    use std::thread;
+    if BIG_STACK_SIZE {
+        thread::Builder::new()
+            .stack_size(32 * 1024 * 1024)
+            .name("solve".into())
+            .spawn(solve)
+            .unwrap()
+            .join()
+            .unwrap();
+    } else {
+        solve();
+    }
+}
+fn solve() {
+    let N = get!(usize);
+    let primes: Vec<_> = 55555
+        .lower_primes()
+        .into_iter()
+        .filter_map(|x| {
+            if x % 5 == 1 {
+                Some(x.to_string())
+            } else {
+                None
+            }
+        })
+        .take(N)
+        .collect();
+    echo!(primes.join(" "));
+}
+
+pub trait Int:
+    std::ops::Add<Output = Self>
+    + std::ops::Sub<Output = Self>
+    + std::ops::Mul<Output = Self>
+    + std::ops::Div<Output = Self>
+    + std::ops::Rem<Output = Self>
+    + std::hash::Hash
+    + PartialEq
+    + Eq
+    + PartialOrd
+    + Ord
+    + Copy
+{
+    fn zero() -> Self;
+    fn one() -> Self;
+    fn next(self) -> Self;
+    fn prev(self) -> Self;
+    fn sqrt_floor(self) -> Self;
+}
+macro_rules !impl_int_for_numerics {($($t :ty ) *) =>($(impl Int for $t {fn zero () ->Self {0 } fn one () ->Self {1 } fn next (self ) ->Self {self +Self ::one () } fn prev (self ) ->Self {self -Self ::one () } fn sqrt_floor (self ) ->Self {if self <Self ::zero () {return Self ::zero () ;} let two =Self ::one () .next () ;let mut ok =Self ::zero () ;let mut ng =self .next () ;while ng -ok >1 {let mid =(ng +ok ) /two ;if mid *mid <=self {ok =mid ;} else {ng =mid ;} } ok } } ) *) }
+impl_int_for_numerics !(u8 i8 u16 i16 u32 i32 u64 i64 usize isize );
+pub trait Prime<T: Int> {
+    fn lower_primes(&self) -> Vec<T>;
+    fn factorize(&self) -> std::collections::HashMap<T, usize>;
+}
+impl<T> Prime<T> for T
+where
+    T: Int,
+{
+    /// エラトステネスの篩を用いてself以下の素数を求める
+    /// 計算量: O(n log log n)
+    fn lower_primes(&self) -> Vec<T> {
+        let &this = self;
+        let mut v = Vec::new();
+        if this <= T::one() {
+            return v;
+        }
+        let mut deque = std::collections::VecDeque::new();
+        let mut t = T::one().next();
+        while t <= this {
+            deque.push_back(t);
+            t = t.next();
+        }
+        let mut p = match deque.pop_front() {
+            Some(x) => x,
+            None => return v,
+        };
+        v.push(p);
+        while p * p <= this {
+            deque = deque
+                .iter()
+                .filter(|&&x| x % p != T::zero())
+                .cloned()
+                .collect();
+            p = match deque.pop_front() {
+                Some(x) => x,
+                None => return v,
+            };
+            v.push(p);
+        }
+        for n in deque {
+            v.push(n);
+        }
+        v
+    }
+    /// エラトステネスの篩を用いてselfを素因数分解する
+    fn factorize(&self) -> std::collections::HashMap<T, usize> {
+        let mut ret = std::collections::HashMap::new();
+        let primes = self.sqrt_floor().lower_primes();
+        let mut tmp = *self;
+        for prime in primes {
+            while tmp % prime == T::zero() {
+                tmp = tmp / prime;
+                *ret.entry(prime).or_insert(0) += 1;
+            }
+        }
+        if tmp > T::one() {
+            *ret.entry(tmp).or_insert(0) += 1;
+        }
+        ret
+    }
+}
